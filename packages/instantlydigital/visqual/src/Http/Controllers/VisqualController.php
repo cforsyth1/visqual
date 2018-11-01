bd1dad8dc96bd40ffbc2dccb234ac4f6dee125f7
caitlin_forsyth <caitlinsophia25@gmail.com>
2018-11-01 22:30:44 +0000

    Get all models in the application

--- packages/instantlydigital/visqual/src/Http/Controllers/VisqualController.php
+++ packages/instantlydigital/visqual/src/Http/Controllers/VisqualController.php
@@ -6,7 +6,25 @@ use App\Http\Controllers\Controller;
 
 class VisqualController extends Controller
 {
+
+    private function getModels($path = false){
+        $path = $path ?: app_path() . env('VISQUAL_MODEL_DIR', "/Models");
+        $out = [];
+        $results = scandir($path);
+        foreach ($results as $result) {
+            if ($result === '.' or $result === '..') continue;
+            $filename = $path . '/' . $result;
+            if (is_dir($filename)) {
+                $out = array_merge($out, $this->getModels($filename));
+            }else{
+                $out[] = substr($filename,0,-4);
+            }
+        }
+        return $out;
+    }
+
     public function index() {
-        return view('visqual::index');
+        $models = $this->getModels();
+        return view('visqual::index', compact('models'));
     }
 }
